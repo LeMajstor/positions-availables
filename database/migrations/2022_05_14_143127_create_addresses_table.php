c1a934197f6ae50ace92e5e303c184c4e0a02db3
LeMajstor <edinaldoprofi99@gmail.com>
2022-05-14 13:24:30 -0300

    adicionando migrations e models

--- database/migrations/2022_05_14_143127_create_addresses_table.php
+++ database/migrations/2022_05_14_143127_create_addresses_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('addresses', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('street');
+            $table->smallInteger('number');
+            $table->string('city');
+            $table->string('district');
+            $table->string('country');
+            $table->timestamps();
         });
     }
 
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('addresses');
     }
 };
---
+++ database/migrations/2022_05_14_144343_create_positions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('positions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('companies_id');
+            $table->foreign('companies_id')
+                ->references('id')
+                ->on('companies');
+            $table->string('title');
+            $table->longText('content');
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('positions');
+    }
+};
